8fd147aa79b45bc1cbdadf8ccdb996642e684923
emam3 <67537653+emam3@users.noreply.github.com>
2020-10-21 23:26:48 +0200

    Add files via upload

---
+++ precache-manifest.615436326774706dce4b614e7753eb15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb608f1327ee6f1150f40da99d2c81b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a595da5cb4eb1fee6a2",
+    "url": "/static/css/2.26884936.chunk.css"
+  },
+  {
+    "revision": "de0c0c934b3e91ceb497",
+    "url": "/static/css/main.1a6008d4.chunk.css"
+  },
+  {
+    "revision": "9a595da5cb4eb1fee6a2",
+    "url": "/static/js/2.d1c85679.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/static/js/2.d1c85679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0c0c934b3e91ceb497",
+    "url": "/static/js/main.15b5d9b5.chunk.js"
+  },
+  {
+    "revision": "7ddcf085f5982b6845ea",
+    "url": "/static/js/runtime-main.37e0861d.js"
+  }
+]);
